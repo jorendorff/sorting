d830ce3e74d5ddd54b6c476a65f69a51cdeacf16
Jason Myers <jason@jasonamyers.com>
2013-08-22 11:21:46 -0500

    Showing each scan
    Signed-off-by: Jason Myers <jason@jasonamyers.com>

--- bubble_sort.py
+++ bubble_sort.py
@@ -21,8 +21,16 @@ for curstep in range(len(samples)-1, 0, -1):
             print(Fore.RED + output)
         else:
             print(Fore.RESET + output)
-    sleep(0.25)
+    sleep(0.15)
     for i in range(curstep):
+        os.system('cls' if os.name=='nt' else 'clear')
+        for idxx in (range(curstep)):
+            output = '{0}, {1}'.format(samples[idxx], samples[idxx]*'|')
+            if i == idxx:
+                print(Fore.GREEN + output)
+            else:
+                print(Fore.RESET + output)
+        sleep(0.05)
         if samples[i] < samples[i +1]:
             samples[i], samples[i+1] = samples[i+1], samples[i]
 print samples
