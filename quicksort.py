92fa70e30e1c6d896f140ba3a50f9c48cd4cbae1
Jason Orendorff <jason.orendorff@gmail.com>
2013-08-26 13:17:24 -0500

    Try to make quicksort a little easier to read.

--- quicksort.py
+++ quicksort.py
@@ -1,7 +1,10 @@
 def quicksort(data):
-    def f(start, stop):
+    def sort_slice(start, stop):
         if stop - start < 2:
             return  # nothing to do
+
+        # Choose a pivot. We do the simplest possible thing here, but
+        # see <https://en.wikipedia.org/wiki/Quicksort#Choice_of_pivot>.
         pivot = data[stop - 1]
 
         # Split data[start:stop] into two piles,
@@ -17,14 +20,14 @@ def quicksort(data):
             else:
                 i += 1
 
-        # Swap the pivot to the middle.
+        # Swap the pivot to the middle, between the two piles.
         data[stop - 1] = data[i]
         data[i] = pivot
 
-        # Sort the halves and we're done.
-        f(start, i)
-        f(i + 1, stop)
-    f(0, len(data))
+        # Sort both piles and we're done.
+        sort_slice(start, i)
+        sort_slice(i + 1, stop)
+    sort_slice(0, len(data))
 
 if __name__ == '__main__':
     import animation
